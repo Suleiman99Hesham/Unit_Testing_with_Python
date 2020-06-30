53021726be266c836008f74d55a507689fadeb8f
Suleiman99Hesham <suleimanhesham99@gmail.com>
2020-06-30 22:08:32 +0200

    first example of test_case includes using of assertEqual() and assertRaises()

---
+++ unit_test_fundamentals/test_phonebook.py
@@ -0,0 +1,19 @@
+import unittest
+
+from phonebook import PhoneBook
+
+class PhoneBookTest(unittest.TestCase):
+    
+    def test_lookup_by_name(self):
+        phonebook = PhoneBook()
+        phonebook.add("Bob", "12345")
+        number = phonebook.lookup("Bob")
+        self.assertEqual("12345", number)
+
+    def test_missing_name(self):
+        phonebook = PhoneBook()
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing")
+        
+if __name__ == "__main__":
+    unittest.main()
